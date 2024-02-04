f58a1c9cc2da8153454f8ee57585f188a1e895bd
jivan23 <128153095+jivan23@users.noreply.github.com>
2024-02-04 22:02:52 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,38 @@
+class Loops
+{
+    public static void main(String arr[])
+    {
+    System.out.println(" Jai Shree Ram ");
+
+   /* int i =10;
+    do
+    {
+        System.out.println(i);
+        i--;
+    }while(i>=1); 
+    */
+
+    /* 
+    int i =0;
+    while(i<10)
+    {
+        System.out.println(i);
+        i++;
+    }
+    */
+
+    /*
+    int i=0;
+    do{
+        System.out.println(i);
+        i++;
+    }while(i<=10);*/
+
+    //int number = 2;
+    for(int i=1;i<=10;i++)
+    {
+        System.out.println(i*2);
+    }
+}
+
+}
